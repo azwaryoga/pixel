97ee44ddf89848120142767db0079280e8f21185
Azwar <azwaryoga@gmail.com>
2026-04-10 23:36:19 +0700

    add reviews and improve design

---
+++ testimonials-data.js
@@ -0,0 +1,27 @@
+const testimonials = [
+  {
+    name: "Oximasa",
+    role: "Streamer",
+    text: "Azwar was exceptional, incredibly creative, and cooperative with everything I have asked for! I would love to work with him again 100%"
+  },
+  {
+    name: "Brendan H.",
+    role: "Business Owner",
+    text: "Talked through some examples and delivered the vision. Very awesome experience"
+  },
+  {
+    name: "Kerrin C.",
+    role: "Musician",
+    text: "Exactly what I hoped for and more :) I'm so impressed with how quickly this was done, and with such attention to the details I provided. It's like he pulled it straight from my head!"
+  },
+  {
+    name: "Vadim",
+    role: "Business Owner",
+    text: "As I was still trying to figure out my exact vision, there were certain minor details I wasn't sure about. So I had a lot of specific revision requests where we went back and forth on certain things. He handled it with great patience and professionalism! Really appreciate his pleasant communication and understanding."
+  },
+  {
+    name: "Chris",
+    role: "Content Creator",
+    text: "His work has always showcased the ideas we had in our minds and realised them fantastically in a detailed and creative way. His communication and openness to feedback and adjustments are great, and he is always polite and responsive. We would highly recommend."
+  }
+];
